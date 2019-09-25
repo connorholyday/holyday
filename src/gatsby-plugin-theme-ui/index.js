ceec029319f623e78fc57c5ef6c0ba2514fa45b1
Connor Holyday <connorholyday@gmail.com>
2019-09-26 00:43:43 +0100

    Fix merriweather

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -16,7 +16,7 @@ export default {
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   fonts: {
     body: "Oxygen, system-ui, sans-serif",
-    heading: "Merriweather, inherit",
+    heading: "Merriweather, serif",
     monospace: "Menlo, monospace",
   },
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
