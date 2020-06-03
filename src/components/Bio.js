51c323141a7cea7379716576ba14581cdf929673
Connor Holyday <connorholyday@gmail.com>
2020-06-03 23:37:13 +0100

    Initial commit

---
+++ src/components/Bio.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+
+import { rhythm } from '../utils/typography'
+
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { social } = data.site.siteMetadata
+        return (
+          <div
+            style={{
+              display: `flex`,
+              marginBottom: rhythm(2.5),
+            }}
+          >
+            <p>
+              Developer at{' '}
+              <a
+                href="https://aranja.com"
+                style={{
+                  boxShadow: `none`,
+                  textDecoration: `none`,
+                  fontWeight: '900',
+                }}
+              >
+                aranja
+              </a>
+              <br />
+              Attempting to spark joy on the internet
+            </p>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+const bioQuery = graphql`
+  query BioQuery {
+    site {
+      siteMetadata {
+        social {
+          twitter
+        }
+      }
+    }
+  }
+`
+
+export default Bio
---
+++ src/components/code.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { useMDXScope } from 'gatsby-plugin-mdx/context'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/nightOwl'
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+
+export const Code = ({ codeString, language, ...props }) => {
+  const components = useMDXScope()
+  if (props['react-live']) {
+    return (
+      <LiveProvider code={codeString} scope={components} theme={theme}>
+        <LiveEditor />
+        <LiveError />
+        <LivePreview />
+      </LiveProvider>
+    )
+  } else {
+    return (
+      <Highlight
+        {...defaultProps}
+        code={codeString}
+        language={language}
+        theme={theme}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre
+            className={className}
+            style={{
+              ...style,
+              padding: '0.5rem 1rem',
+              borderRadius: 2,
+            }}
+          >
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    )
+  }
+}
