647a4c0ba71de60e56e80d49daa85891f3801a5d
Connor Holyday <connorholyday@gmail.com>
2020-08-27 18:09:22 +0100

    Add lab intro

--- src/components/Lab/Lab.js
+++ src/components/Lab/Lab.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { useTrail, animated } from 'react-spring'
+import { useSpring, useTrail, animated } from 'react-spring'
 
 import { rhythm } from '../../utils/typography'
 import Raise from '../Raise'
 import Article from '../Article'
 import { TRANSITION_DELAY_IN_MS } from '../constants'
+import Animate from '../animate'
+import styles from './Lab.module.css'
 
 const Lab = ({ transition, posts, images }) => {
   const [toggle, set] = React.useState(true)
+  React.useEffect(() => {
+    if (transition === 'exiting') {
+      set(false)
+    }
+  }, [transition])
+  const { y, opacity } = useSpring({
+    delay: TRANSITION_DELAY_IN_MS + 0.3,
+    y: toggle ? 0 : -5,
+    opacity: toggle ? 1 : 0,
+    from: { y: 5, opacity: 0 },
+  })
   const trail = useTrail(posts.length, {
     config: {
       duration: TRANSITION_DELAY_IN_MS,
@@ -17,17 +30,18 @@ const Lab = ({ transition, posts, images }) => {
     opacity: toggle ? 1 : 0,
     from: { yr: [5, 2], opacity: 0 },
   })
-  React.useEffect(() => {
-    if (transition === 'exiting') {
-      set(false)
-    }
-  }, [transition])
 
   return (
     <>
-      <h1 style={{ margin: `0 0 ${rhythm(1)}` }}>
-        <Animate toggle={toggle}>The Lab</Animate>
-      </h1>
+      <div className={styles.content}>
+        <h1 style={{ margin: `0 0 ${rhythm(1)}` }}>
+          <Animate toggle={toggle}>Welcome to the Lab</Animate>
+        </h1>
+        <animated.p style={{
+          transform: y.interpolate(y => `translate3d(0,${y}%,0)`),
+          opacity,
+        }}>This is where I can share things that I've built purely for the fun of it.<br />Click through and explore!</animated.p>
+      </div>
       {trail.map(({ yr, opacity, ...rest }, index) => (
         <Article
           key={index}
