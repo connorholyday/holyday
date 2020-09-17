428c7fd8e88e975bf4ab5c140246d238e96ebbe9
Connor Holyday <connorholyday@gmail.com>
2020-09-17 21:11:23 +0100

    Remove unused work content

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,13 +17,6 @@ module.exports = {
         name: `blog`,
       },
     },
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     path: `${__dirname}/src/content/work`,
-    //     name: `work`,
-    //   },
-    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- gatsby-node.js
+++ gatsby-node.js
@@ -49,51 +49,6 @@ exports.createPages = async ({ graphql, actions }) => {
       },
     })
   })
-
-  // const works = await graphql(`
-  //   query {
-  //       work: allMdx(
-  //         filter: { fileAbsolutePath: { regex: "/src/content/work/" } }
-  //         sort: { fields: [frontmatter___date], order: DESC }
-  //         limit: 1000
-  //       ) {
-  //         edges {
-  //           node {
-  //             id
-  //             fields {
-  //               slug
-  //             }
-  //             frontmatter {
-  //               title
-  //             }
-  //             body
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `
-  // ).then(res => {
-  //   if (res.errors) {
-  //     throw res.errors
-  //   }
-  //   return res.data;
-  // });
-
-  // Create work posts pages.
-  // works.work.edges.forEach((post, index) => {
-  //   const previous = index === works.work.edges.length - 1 ? null : works.work.edges[index + 1].node
-  //   const next = index === 0 ? null : works.work.edges[index - 1].node
-
-  //   createPage({
-  //     path: post.node.fields.slug,
-  //     component: path.resolve(`./src/templates/work-post.js`),
-  //     context: {
-  //       slug: post.node.fields.slug,
-  //       previous,
-  //       next,
-  //     },
-  //   })
-  // })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
