abf7c358e841b4c8fea346dc62f5cc0e7be206d8
Connor Holyday <connorholyday@gmail.com>
2026-07-23 16:15:40 +0100

    Remove all files

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import './src/styles/fonts.css'
-
-// custom typefaces
-import 'typeface-open-sans'
-
-import { wrapRootElement as wrap } from './wrap-root-element'
-
-export const wrapRootElement = wrap
--- src/components/Link/index.js
+++
@@ -1,5 +0,0 @@
-export {
-    TransitionLink,
-    InternalLink,
-    ExternalLink,
-} from './Link';
